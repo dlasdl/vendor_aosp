4df38600c9a89292004b5bad9d479836a1f9f1c6
anayw2001 <anayw2001@users.noreply.github.com>
2020-10-21 18:47:21 +0000

    vendor: allow customizing the length of gesture navbar [3/3]
    Change-Id: I9fc95f315d5cf7a2b9a90af9fdce9eadce518653
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/3139
    Tested-by: Andrew Thomas <itz63c@statixos.com>
    Reviewed-by: Andrew Thomas <itz63c@statixos.com>
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -203,4 +203,9 @@ $(call inherit-product, vendor/aosp/config/ota.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Navbar
+PRODUCT_PACKAGES += \
+    GesturalNavigationOverlayLong \
+    GesturalNavigationOverlayMedium
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
